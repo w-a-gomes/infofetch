e7ddbd3bdc2a4c48993190775601dc00edc3de50
w-a-gomes <wildenalisson@outlook.com>
2020-10-28 22:14:00 -0300

    default logo

--- oslogos-.py
+++ oslogos-.py
@@ -85,49 +85,6 @@ class Logo(object):
         yellow = color.get_style(color='yellow')
         reset = color.reset_style()
 
-        default_logo = """
-                {}               .''''''''.               
-                {}              .'''''''''''              
-                {}              ,MN{}'''{}NMN:{}''.             
-                {}              l{}M{}N{}';'{}N{}M{}N{};'''             
-                {}              :{}xXMMMMXxx{}'''             
-                {}             .o{}xMMMMMXx'{},''.            
-                {}             .k{}M{}'xNNK'{}MMd{}'''.           
-                {}           ..{}dMMMMMMMMMMMl{}''''.         
-                {}          .,{}OMMMMMMMMMMMMMd{}'''''.       
-                {}        .'c{}NMMMMMMMMMMMMMMM0{},''''.      
-                {}       .'o{}MMMMMMMMMMMMMMMMMMK{}''''''.    
-                {}      '',{}WMMMMMMMMMMMMMMMMMMM{}:''''''    
-                {}     .::,x{}WMMMMMMMMMMMMMMMMMMc{}'''''.    
-                {}   .:kKKk{}l,d{}NMMMMMMMMMMMMMWO{}xK{}'''':{}kx   
-                {}dkkkkkkkkkx{};'c0{}MMMMMMMMMMMW{}kkkkxxxkkk:  
-                {}kkkkkkkkkkkkd{}:cX{}MMMMMMMMNko{}kkkkkkkkkkkk;
-                {}kkkkkkkkkkkkkko{}:lodddoc{},''{}okkkkkkkkkk'  
-                {}.;kkkkkkkkkkkkk{}.'''''':;..{}kkkkkkkkk'    
-                {}   ''':xkkkkk'           'kkkkx'
-                {}""".format(
-            blue,
-            blue,
-            white, blue, white, blue,
-            white, blue, white, blue, white, blue, white, blue,
-            blue, yellow, blue,
-            blue, yellow, blue,
-            blue, white, yellow, white, blue,
-            blue, white, blue,
-            blue, white, blue,
-            blue, white, blue,
-            blue, white, blue,
-            blue, white, blue,
-            blue, white, blue,
-            yellow, blue, white, yellow, blue, yellow,
-            yellow, blue, white, yellow,
-            yellow, blue, white, yellow,
-            yellow, white, blue, yellow,
-            yellow, blue, yellow,
-            yellow,
-            reset
-        )
-
         if self.__os_id == 'arch-linux':
             self.__accent_color = blue
             return """
@@ -397,7 +354,48 @@ class Logo(object):
 
         elif self.__os_id == 'linux-kernel':
             self.__accent_color = yellow
-            return default_logo
+            return """
+{}               .''''''''.               
+{}              .'''''''''''              
+{}              ,MN{}'''{}NMN:{}''.             
+{}              l{}M{}N{}';'{}N{}M{}N{};'''             
+{}              :{}xXMMMMXxx{}'''             
+{}             .o{}xMMMMMXx'{},''.            
+{}             .k{}M{}'xNNK'{}MMd{}'''.           
+{}           ..{}dMMMMMMMMMMMl{}''''.         
+{}          .,{}OMMMMMMMMMMMMMd{}'''''.       
+{}        .'c{}NMMMMMMMMMMMMMMM0{},''''.      
+{}       .'o{}MMMMMMMMMMMMMMMMMMK{}''''''.    
+{}      '',{}WMMMMMMMMMMMMMMMMMMM{}:''''''    
+{}     .::,x{}WMMMMMMMMMMMMMMMMMMc{}'''''.    
+{}   .:kKKk{}l,d{}NMMMMMMMMMMMMMWO{}xK{}'''':{}kx   
+{}dkkkkkkkkkx{};'c0{}MMMMMMMMMMMW{}kkkkxxxkkk:  
+{}kkkkkkkkkkkkd{}:cX{}MMMMMMMMNko{}kkkkkkkkkkkk;
+{}kkkkkkkkkkkkkko{}:lodddoc{},''{}okkkkkkkkkk'  
+{}.;kkkkkkkkkkkkk{}.'''''':;..{}kkkkkkkkk'    
+{}   ''':xkkkkk'           'kkkkx'
+{}""".format(
+                blue,
+                blue,
+                white,  blue,   white,  blue,
+                white,  blue,   white,  blue,   white,  blue,   white,  blue,
+                blue,   yellow, blue,
+                blue,   yellow, blue,
+                blue,   white,  yellow, white,  blue,
+                blue,   white,  blue,
+                blue,   white,  blue,
+                blue,   white,  blue,
+                blue,   white,  blue,
+                blue,   white,  blue,
+                blue,   white,  blue,
+                yellow, blue,   white,  yellow,  blue,   yellow,
+                yellow, blue,   white,  yellow,
+                yellow, blue,   white,  yellow,
+                yellow, white,  blue,   yellow,
+                yellow, blue,   yellow,
+                yellow,
+                reset
+            )
 
         elif self.__os_id == 'linux-mint':
             self.__accent_color = green
@@ -790,10 +788,6 @@ class Logo(object):
                 blue,
                 reset
             )
-
-        else:
-            self.__accent_color = yellow
-            return default_logo
 
 
 if __name__ == '__main__':
